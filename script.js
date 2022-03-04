dff6e3708c73601bd91e4730e867038981bfc57d
William Neves <drwilliamneves@gmail.com>
2022-03-03 22:32:11 -0500

    pac-man-renewed

---
+++ script.js
@@ -0,0 +1,103 @@
+// WORLD MAP
+var world = [
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    ]
+var worldType = {
+    0: "path",
+    1: "coin",
+    2: "brick"
+}
+
+function drawWorld() {
+    var content = ""
+    for(row in world){
+        content += '<div class="row">\n';
+        for(col in world[row]){
+            content += `<div class="${worldType[world[row][col]]}"></div>\n`;
+        }
+        content += '</div>\n';
+        console.log(content)
+    }
+    document.getElementById('world-map').innerHTML = content
+}
+drawWorld()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// var user = "DAY"
+// var score = "000"
+// const content = user+'-'+score+'|'
+// var historyScore = ""
+
+
+
+
+
+
+// // fs.appendFile("score.txt", content, (err) => {
+// //     if(err) throw err;
+// // })
+// var info
+
+// info = fs.readFileSync('score.txt','utf8',(err,data) => {
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     //console.log(data);
+// })
+// info = info.split('|')
+
+// console.log(info, info.length)
