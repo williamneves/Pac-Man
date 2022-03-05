57f281540a5e83cf101a831de2fc1deeda7ee01a
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-03-05 00:08:39 -0500

    Ghosts created
    Ghosts add to html and funciton. Collides also creates, but problem with removing the ghosts.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // WORLD MAP
 var world = [
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    [2,0,1,1,1,1,1,1,1,2,1,1,2,3,1,1,1,1,1,1,1,1,0,2],
-    [2,1,2,2,1,2,2,1,1,2,1,1,2,2,1,2,1,2,2,2,1,2,1,2],
-    [2,1,2,3,1,0,2,1,1,2,1,1,1,1,1,1,1,2,3,1,1,2,1,2],
-    [2,1,2,2,1,2,2,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,1,2],
-    [2,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,2,1,1,1,1,1,0,1,1,1,1,2,1,1,1,1,1,2],
-    [2,2,2,1,1,2,2,2,1,1,1,2,2,1,1,1,2,2,2,1,1,2,2,2],
-    [2,1,1,1,1,1,2,1,1,1,1,0,1,1,1,1,1,2,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,2,1,2,1,1,2,1,1,1,1,1,1,1,1,2],
-    [2,1,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,2,2,1,2,2,1,2],
-    [2,1,2,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,3,1,3,2,1,2],
-    [2,1,2,2,1,2,2,2,1,2,1,2,1,1,2,1,1,2,2,1,2,2,1,2],
-    [2,3,1,1,1,2,3,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,0,2],
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    ]
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2],
+    [2, 1, 2, 2, 1, 2, 2, 1, 1, 2, 1, 1, 2, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2],
+    [2, 1, 2, 3, 1, 0, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 3, 1, 1, 2, 1, 2],
+    [2, 1, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+    [2, 2, 2, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 1, 1, 2, 2, 2],
+    [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 1, 2, 2, 1, 2],
+    [2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 3, 1, 3, 2, 1, 2],
+    [2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 2, 1, 2, 2, 1, 2],
+    [2, 3, 1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+]
 var worldType = { //world types
     0: "path",
     1: "coin",
@@ -23,9 +23,9 @@ var worldType = { //world types
     3: "cherry"
 }
 // draw characters
-var pacman = {x:1,y:1,lifes:3};
-var ghosts = {1:{x:15,y:1,lifes:1},2:{x:15,y:5,lifes:1}, 3:{x:15,y:10,lifes:1},4:{x:15,y:15,lifes:1}};
-console.log('1',Object.keys(ghosts).length)
+var pacman = { x: 1, y: 1, lives: 3};
+var ghosts = { n:4, 1: { x: 22, y: 1, lives: 1 }, 2: { x: 12, y: 6, lives: 1 }, 3: { x: 11, y: 8, lives: 1 }, 4: { x: 22, y: 13, lives: 1 } };
+//console.log('1',Object.keys(ghosts).length)
 // other variables
 var square = 25 //size of pacman
 var score = 2000
@@ -37,9 +37,9 @@ var timeExp = false
 // Draw the MAP
 function drawWorld() {
     var content = ""
-    for(row in world){
+    for (row in world) {
         content += '<div class="row">\n';
-        for(col in world[row]){
+        for (col in world[row]) {
             content += `<div class="${worldType[world[row][col]]}"></div>\n`;
         }
         content += '</div>\n';
@@ -49,35 +49,80 @@ function drawWorld() {
 drawWorld()
 
 // DRAW THE PACMAN
-function drawPacman(){
-    document.getElementById('pacman').style.top = pacman.y * square + 'px'; 
-    document.getElementById('pacman').style.left = pacman.x * square + 'px'; 
+function drawPacman() {
+    document.getElementById('pacman').style.top = pacman.y * square + 'px';
+    document.getElementById('pacman').style.left = pacman.x * square + 'px';
 }
 drawPacman()
 
 // Draw the Ghosts
+function drawGhosts() {
+    if (ghosts[1].lives > 0) {
+        document.getElementById('ghost1').style.left = ghosts[1].x * square + 'px'
+        document.getElementById('ghost1').style.top = ghosts[1].y * square + 'px'
+    }
+    else{
+        document.getElementById('ghost1').style.display = 'none'
+    }
+    if (ghosts[2].lives > 0) {
+        document.getElementById('ghost2').style.left = ghosts[2].x * square + 'px'
+        document.getElementById('ghost2').style.top = ghosts[2].y * square + 'px'
+    }
+    else{
+        document.getElementById('ghost2').style.display = 'none'
+    }
+    if (ghosts[3].lives > 0) {
+        document.getElementById('ghost3').style.left = ghosts[3].x * square + 'px'
+        document.getElementById('ghost3').style.top = ghosts[3].y * square + 'px'
+    }
+    else{
+        document.getElementById('ghost3').style.display = 'none'
+    }
+    if (ghosts[4].lives > 0) {
+        document.getElementById('ghost4').style.left = ghosts[4].x * square + 'px'
+        document.getElementById('ghost4').style.top = ghosts[4].y * square + 'px'
+    }
+    else{
+        document.getElementById('ghost4').style.display = "none"
+    }
+}
+drawGhosts()
 
+// PacMan and Ghost collide function
+function pacGhostColl() {
+    for (var i = 1; i <= 4; i++) {
+        if (pacman.x == ghosts[i].x && pacman.y == ghosts[i].y){
+            console.log(pacman.x,pacman.y,ghosts[i].lives,pacman.lives,"lost live");
+            ghosts[i].lives--;
+            pacman.lives--;
+            drawLifes()
+            drawGhosts()
+        }
+    }
+}
 
 // draw the score
-function drawScore(){
+function drawScore() {
     document.getElementById('scores').innerText = score;
 }
 drawScore()
 
 // Draw the lifes
-function drawLifes(){
-    for (i in pacman.lifes){
-        document.getElementById('lifes').innerHTML = '<img src="pacman-life.ico" alt="pacman lifes">';
+function drawLifes() {
+    content = ""
+    for (var i = 0; i < pacman.lives;i++) {
+        content += '<img src="pacman-life.ico" alt="pacman lifes">\n';
     }
+    document.getElementById('lives').innerHTML = content;
 }
 drawLifes()
 
 //Draw the cherrys
-function drawCherry(){
+function drawCherry() {
     count = 0
-    for (i in world){
-        for (y in world[i]){
-            if (world[i][y] == 3){
+    for (i in world) {
+        for (y in world[i]) {
+            if (world[i][y] == 3) {
                 count++;
             }
         }
@@ -86,15 +131,15 @@ function drawCherry(){
 }
 drawCherry()
 
-function setTimer() {
+async function setTimer() {
     if (triggerTimer && timer == 90) {
         myfunc = setInterval(function () {
-            if (timer != 0){
+            if (timer != 0) {
                 timer--;
-                score-=22;
+                score -= 22;
                 drawScore();
             }
-            else{
+            else {
                 timeExp = true;
                 drawScore();
             }
@@ -103,50 +148,51 @@ function setTimer() {
     }
 }
 
-document.onkeydown = function(key){
+document.onkeydown = function (key) {
 
-    if (key.keyCode == 37){
-        if (world[pacman.y][pacman.x - 1] != 2){ // LEFT
+    if (key.keyCode == 37) {
+        if (world[pacman.y][pacman.x - 1] != 2) { // LEFT
             pacman.x--;
             document.getElementById('pacman').style.transform = "scaleX(-1)"
             triggerTimer = true;
         }
     }
-    if (key.keyCode == 39){
-        if (world[pacman.y][pacman.x + 1] != 2){ // RIGHT
+    if (key.keyCode == 39) {
+        if (world[pacman.y][pacman.x + 1] != 2) { // RIGHT
             pacman.x++;
             document.getElementById('pacman').style.transform = "scaleX(1)"
             triggerTimer = true;
         }
     }
-    if (key.keyCode == 38){
-        if (world[pacman.y - 1][pacman.x] != 2){ // UP
+    if (key.keyCode == 38) {
+        if (world[pacman.y - 1][pacman.x] != 2) { // UP
             pacman.y--;
             document.getElementById('pacman').style.transform = "rotate(-90deg)"
             triggerTimer = true;
         }
     }
-    if (key.keyCode == 40){
-        if (world[pacman.y + 1][pacman.x] != 2){ // DOWN
+    if (key.keyCode == 40) {
+        if (world[pacman.y + 1][pacman.x] != 2) { // DOWN
             pacman.y++;
             document.getElementById('pacman').style.transform = "rotate(90deg)"
             triggerTimer = true;
         }
     }
 
-    if (world[pacman.y][pacman.x] == 0){
+    if (world[pacman.y][pacman.x] == 0) {
     }
 
-    if (world[pacman.y][pacman.x] == 1){
+    if (world[pacman.y][pacman.x] == 1) {
         score += 15
         world[pacman.y][pacman.x] = 0
     }
 
-    if (world[pacman.y][pacman.x] == 3){
+    if (world[pacman.y][pacman.x] == 3) {
         score += 50
         world[pacman.y][pacman.x] = 0
     }
-
+    drawGhosts()
+    pacGhostColl()
     drawScore()
     drawLifes()
     drawCherry()
