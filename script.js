6c8e13a1ab492e1ec6161f82ed148c30b185d50f
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-03-04 21:48:46 -0500

    More Code

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 // WORLD MAP
 var world = [
     [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-    [2,0,1,1,1,1,1,1,1,2,1,1,2,1,1,1,1,1,1,1,1,1,0,2],
+    [2,0,1,1,1,1,1,1,1,2,1,1,2,3,1,1,1,1,1,1,1,1,0,2],
     [2,1,2,2,1,2,2,1,1,2,1,1,2,2,1,2,1,2,2,2,1,2,1,2],
-    [2,1,2,0,1,0,2,1,1,2,1,1,1,1,1,1,1,2,0,1,1,2,1,2],
+    [2,1,2,3,1,0,2,1,1,2,1,1,1,1,1,1,1,2,3,1,1,2,1,2],
     [2,1,2,2,1,2,2,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,1,2],
     [2,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,2,1,1,1,1,0,0,1,1,1,1,2,1,1,1,1,1,2],
+    [2,1,1,1,1,1,2,1,1,1,1,1,0,1,1,1,1,2,1,1,1,1,1,2],
     [2,2,2,1,1,2,2,2,1,1,1,2,2,1,1,1,2,2,2,1,1,2,2,2],
-    [2,1,1,1,1,1,2,1,1,1,1,0,0,1,1,1,1,2,1,1,1,1,1,2],
+    [2,1,1,1,1,1,2,1,1,1,1,0,1,1,1,1,1,2,1,1,1,1,1,2],
     [2,1,1,1,1,1,1,1,1,2,1,2,1,1,2,1,1,1,1,1,1,1,1,2],
     [2,1,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,2,2,1,2,2,1,2],
-    [2,1,2,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,0,1,0,2,1,2],
+    [2,1,2,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,3,1,3,2,1,2],
     [2,1,2,2,1,2,2,2,1,2,1,2,1,1,2,1,1,2,2,1,2,2,1,2],
-    [2,0,1,1,1,2,0,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,0,2],
+    [2,3,1,1,1,2,3,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,0,2],
     [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
     ]
-var worldType = {
+var worldType = { //world types
     0: "path",
     1: "coin",
-    2: "brick"
+    2: "brick",
+    3: "cherry"
 }
+// draw characters
+var pacman = {x:1,y:1,lifes:3};
+var ghosts = {1:{x:15,y:1,lifes:1},2:{x:15,y:5,lifes:1}, 3:{x:15,y:10,lifes:1},4:{x:15,y:15,lifes:1}};
+console.log('1',Object.keys(ghosts).length)
+// other variables
+var square = 25 //size of pacman
+var score = 2000
+var cherry = 0 // will be defined on the map
+var timer = 90
+var triggerTimer = false
+var timeExp = false
+
+// Draw the MAP
 function drawWorld() {
     var content = ""
     for(row in world){
@@ -35,64 +49,110 @@ function drawWorld() {
 drawWorld()
 
 // DRAW THE PACMAN
-
-var pacman = { //pacman position relative
-    x: 1,
-    y: 1
-}
-var square = 25 //size of pacman
-
 function drawPacman(){
     document.getElementById('pacman').style.top = pacman.y * square + 'px'; 
     document.getElementById('pacman').style.left = pacman.x * square + 'px'; 
 }
 drawPacman()
 
-// MOVE THE PACMAN
-var score = 0
+// Draw the Ghosts
+
+
+// draw the score
+function drawScore(){
+    document.getElementById('scores').innerText = score;
+}
+drawScore()
+
+// Draw the lifes
+function drawLifes(){
+    for (i in pacman.lifes){
+        document.getElementById('lifes').innerHTML = '<img src="pacman-life.ico" alt="pacman lifes">';
+    }
+}
+drawLifes()
+
+//Draw the cherrys
+function drawCherry(){
+    count = 0
+    for (i in world){
+        for (y in world[i]){
+            if (world[i][y] == 3){
+                count++;
+            }
+        }
+    }
+    document.getElementById("cherrys-num").innerText = count
+}
+drawCherry()
+
+function setTimer() {
+    if (triggerTimer && timer == 90) {
+        myfunc = setInterval(function () {
+            if (timer != 0){
+                timer--;
+                score-=22;
+                drawScore();
+            }
+            else{
+                timeExp = true;
+                drawScore();
+            }
+            document.getElementById('timer').innerText = timer + 's';
+        }, 1000)
+    }
+}
 
 document.onkeydown = function(key){
+
     if (key.keyCode == 37){
         if (world[pacman.y][pacman.x - 1] != 2){ // LEFT
             pacman.x--;
             document.getElementById('pacman').style.transform = "scaleX(-1)"
+            triggerTimer = true;
         }
     }
     if (key.keyCode == 39){
         if (world[pacman.y][pacman.x + 1] != 2){ // RIGHT
-        pacman.x++;
-        document.getElementById('pacman').style.transform = "scaleX(1)"
+            pacman.x++;
+            document.getElementById('pacman').style.transform = "scaleX(1)"
+            triggerTimer = true;
         }
     }
     if (key.keyCode == 38){
         if (world[pacman.y - 1][pacman.x] != 2){ // UP
-        pacman.y--;
-        document.getElementById('pacman').style.transform = "rotate(-90deg)"
+            pacman.y--;
+            document.getElementById('pacman').style.transform = "rotate(-90deg)"
+            triggerTimer = true;
         }
     }
-    
     if (key.keyCode == 40){
         if (world[pacman.y + 1][pacman.x] != 2){ // DOWN
-        pacman.y++;
-        document.getElementById('pacman').style.transform = "rotate(90deg)"
+            pacman.y++;
+            document.getElementById('pacman').style.transform = "rotate(90deg)"
+            triggerTimer = true;
         }
     }
+
     if (world[pacman.y][pacman.x] == 0){
-        console.log('clear');
     }
+
     if (world[pacman.y][pacman.x] == 1){
-        console.log('coin')
         score += 15
         world[pacman.y][pacman.x] = 0
     }
+
     if (world[pacman.y][pacman.x] == 3){
-        console.log('cherry')
         score += 50
         world[pacman.y][pacman.x] = 0
     }
-    console.log(score)
+
+    drawScore()
+    drawLifes()
+    drawCherry()
     drawWorld()
     drawPacman()
+    setTimer()
 }
 
 
